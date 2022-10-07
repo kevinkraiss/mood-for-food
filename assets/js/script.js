e47fce5de0c97fa1cc63b3a87fe681db785a2336
Kevin Kraiss <kevinkraiss@gmail.com>
2022-10-06 20:51:57 -0500

    adds js for horroscope api

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,38 @@ console.log("hello world")
 var userBirthdayForm = document.querySelector('form')
 var userBirthdayInput = document.getElementById('userBirthday')
 
-
+// handles user birthday input
 function handleBirthday(event) {
     event.preventDefault()
 
     var birthday = userBirthdayInput.value
     console.log(birthday)
 
-    var reformatDate = moment(birthday, "MM-DD-YY").format("dddd, MMMM Do YYYY")
+    var reformatDate = moment(birthday, "YYYY-MM-DD").format("dddd, MMMM Do YYYY")
 
 
     console.log(reformatDate)
 
 }
+
+// gets horroscope info from aztro API
+function getHscope() {
+    var requestUrl = 'https://aztro.sameerkumar.website/?sign=aries&day=today'
+
+    fetch(requestUrl)
+        .then(function (response) {
+            return response
+            console.log(response)
+        })
+        .then(function(data) {
+            console.log(data)
+            
+        })
+    
+        
+    }
+getHscope()
+
 userBirthdayForm.addEventListener('submit', handleBirthday)
 
 
